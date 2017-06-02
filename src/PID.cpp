b8368906ad10aa8ab7e8c1dba47413cd344d3c9a
eigendreams <jakob.culebro@gmail.com>
2017-06-01 20:25:07 -0500

    tuned parameters and test in simulation

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <cmath>
 
 using namespace std;
 
@@ -11,11 +12,26 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+
+	p_error = 0;
+	i_error = 0;
+	d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+	d_error = 0.95 * (cte - p_error - d_error) + (d_error);
+
+	p_error = cte;
+	i_error += cte;
+	
+	if ( std::abs(Ki * i_error) > 10 * M_PI / 180 )
+		i_error = std::copysign(1.0, i_error) * ( 10 * M_PI / 180 ) / Ki;
 }
 
 double PID::TotalError() {
+	return -Kp * p_error - Ki * i_error - Kd * d_error;
 }
 
